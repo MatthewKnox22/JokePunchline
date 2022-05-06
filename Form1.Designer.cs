3a3bb0cb6ed21509494aefc6d6ae7b5677a8bae9
Matthew S Knox <91904135+MatthewKnox22@users.noreply.github.com>
2022-05-06 10:54:52 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace JokePunchline
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SetupBtn = new System.Windows.Forms.Button();
+            this.PunchLineBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(242, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Why didn\'t the skeleton cross the road? ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // SetupBtn
+            // 
+            this.SetupBtn.Location = new System.Drawing.Point(245, 234);
+            this.SetupBtn.Name = "SetupBtn";
+            this.SetupBtn.Size = new System.Drawing.Size(75, 23);
+            this.SetupBtn.TabIndex = 1;
+            this.SetupBtn.Text = "Setup";
+            this.SetupBtn.UseVisualStyleBackColor = true;
+            this.SetupBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PunchLineBtn
+            // 
+            this.PunchLineBtn.Location = new System.Drawing.Point(430, 234);
+            this.PunchLineBtn.Name = "PunchLineBtn";
+            this.PunchLineBtn.Size = new System.Drawing.Size(75, 23);
+            this.PunchLineBtn.TabIndex = 2;
+            this.PunchLineBtn.Text = "Punchline";
+            this.PunchLineBtn.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(470, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.Location = new System.Drawing.Point(677, 388);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(75, 23);
+            this.ExitBtn.TabIndex = 4;
+            this.ExitBtn.Text = "Exit";
+            this.ExitBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ExitBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PunchLineBtn);
+            this.Controls.Add(this.SetupBtn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button SetupBtn;
+        private System.Windows.Forms.Button PunchLineBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ExitBtn;
+    }
+}
+
